a8c0b2b453ed9ca22b3e941be6f19456d0490a1e
nithzhizn-cpu <nithzhizn@gmail.com>
2025-11-17 17:03:32 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./data.db"
+DATABASE_URL = "sqlite:///./spysignal.db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
